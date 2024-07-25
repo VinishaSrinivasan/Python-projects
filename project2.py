0df924e85d92dd3592c16e2435354cd64dec2846
VinishaSrinivasan <vinishasrini15@gmail.com>
2024-07-25 21:21:23 +0530

    dice roll project completed

---
+++ project2.py
@@ -0,0 +1,16 @@
+# RANDOM PASSWORD GENRATOR #
+
+import random
+
+print("Welcome to random password generater ! ")
+randchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#@$*"
+nopwdnd = int(input("Please enter the number of passwords to be generated: "))
+pwdlen = int(input("Please enter the length of the password: "))
+
+print("Here are your random passwords: ")
+
+for v in range(nopwdnd):
+    pwd=""
+    for j in range(pwdlen):
+        pwd=pwd+random.choice(randchar)
+    print(pwd) 
